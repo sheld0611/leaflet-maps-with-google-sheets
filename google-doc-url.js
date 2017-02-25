f7e5ffe51bfd105a4044ae06b2b46b54a34656bd
sheld0611 <sheld@dialrealtycorp.com>
2017-02-25 14:31:09 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1whsDOpBdn4IR6BHggiUZwelpnseThSjAR9yFPFEStMA/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1XYgw-HtS30g0ly86dftbU9A5DNbigob6cU_zV5wkGow/pubhtml';
